7cdf087a0252a257ed5137f39cb846737f83da16
abhaypatel03 <abhaypa840170@gmail.com>
2026-04-10 10:15:57 +0530

    added home page

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,9 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("🔥 My Django site is LIVE 😎")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls')),
+    path('', home),   # 👈 ye important hai
+   
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
