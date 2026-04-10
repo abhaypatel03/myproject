49fe4915a87a3d779a178ec4c2fac5e1602008b6
abhaypatel03 <abhaypa840170@gmail.com>
2026-04-10 10:31:58 +0530

    set myapp as homepage

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,17 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'index.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', home),   # ✅ HOME PAGE (IMPORTANT)
-
-    path('myapp/', include('myapp.urls')),  # ✅ app routes
+    path('', include('myapp.urls')),   # ✅ only this
 
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
